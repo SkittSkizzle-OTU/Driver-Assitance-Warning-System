7c066477c00eb37a1ec6148d5b33167189dd4009
SkittSkizzle-OTU <devin.stephenson@ontariotechu.net>
2025-03-23 14:41:30 -0400

    Update testMRRData.py
    Fixed comparing list to int, so every entry is tested instead of the object

--- Python Radar Data Test/testMRRData.py
+++ Python Radar Data Test/testMRRData.py
@@ -26,27 +26,28 @@ try:#Make room for interrupts
         doppler = [x * 10 ** -38 if i % 2 != 0 else x for i, x in enumerate(doppler)]
         doppler = [x * 10 ** 42 if i % 2 == 0 else x for i, x in enumerate(doppler)]
         time = [abs(x) for x in [a / b for a, b in zip(range, doppler)]]
-        match time:                #Match case switch
-            case _ if time>=7:#If time is greater than 7 seconds switch off everything and enable green light
-                red.off()
-                yellow.off()
-                green.on()
-                speakerLow.stop()
-                speakerHigh.stop()                
-                
-            case _ if time>=6.6 and time<7:#If time is between 7 and 6.6 seconds, enable yellow light and low tone switch off else
-                red.off()
-                yellow.on()
-                green.off()
-                speakerLow.play(220.0)
-                speakerHigh.stop()
-        
-            case _ if time<6.6 and time != 0:#If time is less than 6.6 seconds, enable red light and high tone switch off else
-                red.on()
-                yellow.off()
-                green.off()
-                speakerLow.stop()
-                speakerHigh.play(220.0)
+        for t in time:
+            match t:                #Match case switch
+                case _ if t>=7:#If time is greater than 7 seconds switch off everything and enable green light
+                    red.off()
+                    yellow.off()
+                    green.on()
+                    speakerLow.stop()
+                    speakerHigh.stop()
+
+                case _ if 6.6 <= t < 7:#If time is between 7 and 6.6 seconds, enable yellow light and low tone switch off else
+                    red.off()
+                    yellow.on()
+                    green.off()
+                    speakerLow.play(220.0)
+                    speakerHigh.stop()
+
+                case _ if t<6.6 and t != 0:#If time is less than 6.6 seconds, enable red light and high tone switch off else
+                    red.on()
+                    yellow.off()
+                    green.off()
+                    speakerLow.stop()
+                    speakerHigh.play(220.0)
 except KeyboardInterrupt:#If force closed with ctrl+c turn everything off
     red.off()
     yellow.off()
